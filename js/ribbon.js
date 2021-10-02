1975ce3144cca1a216515b824b1ada7623fedcff
weilaimg1314 <weilaimg1314@gmail.com>
2021-10-03 10:42:07 +1100

    edit main function -- construct main table

--- js/ribbon.js
+++ js/ribbon.js
@@ -154,6 +154,17 @@ function OnGetEnabled(control) {
                 return bFlag
                 break
             }
+        case "New_Chara":{
+            let bFlag = wps.PluginStorage.getItem("EnableFlag")
+            return bFlag
+            break
+        }
+        case "btnShowTaskPane":{
+            let bFlag = wps.PluginStorage.getItem("EnableFlag")
+            return bFlag
+            break
+        }
+
         default:
             break
     }
@@ -185,4 +196,143 @@ function OnGetLabel(control){
 
 function OnNewDocumentApiEvent(doc){
     alert("新建文件事件响应，取文件名: " + doc.Name)
+}
+
+
+function WAMPO_init(){
+
+    if (Application.ActiveDocument.Tables.Item(1)){
+        // var chara_table = Application.ActiveDocument.Tables.Item(1)
+        // console.log(chara_table.Cell(1,1).Width=20)
+        if(Application.ActiveDocument.Tables.Item(1).Columns.Count == 5){
+
+            var chara_table = Application.ActiveDocument.Tables.Item(1)
+            chara_table.Cell(1,1).Range.Select()
+            var header1 = Application.Selection.Text.replace( /^\s+|\s+$/g, "" );
+            chara_table.Cell(1,2).Range.Select()
+            var header2 = Application.Selection.Text.replace( /^\s+|\s+$/g, "" );
+            chara_table.Cell(1,3).Range.Select()
+            var header3 = Application.Selection.Text.replace( /^\s+|\s+$/g, "" );
+            chara_table.Cell(1,4).Range.Select()
+            var header4 = Application.Selection.Text.replace( /^\s+|\s+$/g, "" );
+            chara_table.Cell(1,5).Range.Select()
+            var header5 = Application.Selection.Text.replace( /^\s+|\s+$/g, "" );
+
+            if (header1=="角色名称" && header2=="CV名称" && header3=="角色描述" && header4=="角色性别" && header5=="着色预览"){
+                Application.Selection.EndKey(Application.Enum.wdLine,Application.Enum.wdMove)
+                return true    
+            } else {
+                var con = confirm("人物属性表存在问题，无法正确解析，是否重新创建？");
+                if(con == true){
+                    Construct_Main_Form();
+                    Application.Selection.EndKey(Application.Enum.wdLine,Application.Enum.wdMove)
+                    return true
+                } 
+            }
+        }
+    } else { 
+
+        var con = confirm("未检测到人物属性表，是否立即创建？");
+        if(con == true){
+            Construct_Main_Form();
+            Application.Selection.EndKey(Application.Enum.wdLine,Application.Enum.wdMove)
+            return true
+        }
+        return false
+
+    }
+}
+
+function Construct_Main_Form(){
+
+    Application.ActiveDocument.Range(0, 0).InsertBreak()
+    Application.ActiveDocument.Range(0, 0).Select()
+    Application.Selection.InsertBefore("* 此表格为画本工具自动生成，包含关键配置，任意修改可能导致程序混乱")
+    Application.Selection.Expand(4)
+    Application.Selection.Font.Size = 8
+    Application.Selection.Font.Bold = -1
+    Application.Selection.ParagraphFormat.Alignment = Application.Enum.wdAlignParagraphLeft;
+    Application.Selection.Font.Color = 0x0
+
+    let tblNew = Application.ActiveDocument.Tables.Add(Application.ActiveDocument.Range(0, 0), 1, 5)
+    tblNew.Cell(1, 1).Range.InsertAfter("角色名称")
+    tblNew.Cell(1, 1).Range.Select()
+    Application.Selection.Font.Bold = 0
+    Application.Selection.Font.Color = 0x666666 
+    Application.Selection.Font.Size = 10
+    Application.Selection.ParagraphFormat.Alignment = Application.Enum.wdAlignParagraphCenter;
+
+    tblNew.Cell(1, 2).Range.InsertAfter("CV名称")
+    tblNew.Cell(1, 2).Range.Select()
+    Application.Selection.Font.Color = 0x666666 
+    Application.Selection.Font.Size = 10
+    Application.Selection.ParagraphFormat.Alignment = Application.Enum.wdAlignParagraphCenter;
+
+    tblNew.Cell(1, 3).Range.InsertAfter("角色描述")
+    tblNew.Cell(1, 3).Range.Select()
+    Application.Selection.Font.Color = 0x666666 
+    Application.Selection.Font.Size = 10
+    Application.Selection.ParagraphFormat.Alignment = Application.Enum.wdAlignParagraphCenter;
+
+    tblNew.Cell(1, 4).Range.InsertAfter("角色性别")
+    tblNew.Cell(1, 4).Range.Select()
+    Application.Selection.Font.Color = 0x666666 
+    Application.Selection.Font.Size = 10
+    Application.Selection.ParagraphFormat.Alignment = Application.Enum.wdAlignParagraphCenter;
+
+    tblNew.Cell(1, 5).Range.InsertAfter("着色预览")
+    tblNew.Cell(1, 5).Range.Select()
+    Application.Selection.Font.Color = 0x666666 
+    Application.Selection.Font.Size = 10
+    Application.Selection.ParagraphFormat.Alignment = Application.Enum.wdAlignParagraphCenter;
+
+    // tblNew.Columns.AutoFit()
+    let myTable = Application.ActiveDocument.Tables.Item(1)
+    myTable.Borders.InsideLineStyle = Application.Enum.wdLineStyleDashLargeGap
+    myTable.Borders.OutsideLineStyle = Application.Enum.wdLineStyleSingle
+
+    myTable.Cell(1,1).Width=60
+    myTable.Cell(1,2).Width=60
+    myTable.Cell(1,3).Width=200
+    myTable.Cell(1,4).Width=60
+    myTable.Cell(1,5).Width=60
+
+}
+
+function WAMPO_MAIN_ACTION(control){
+    const eleId = control.Id
+    switch (eleId) {
+        case "Startup":
+        {
+            const doc = wps.WpsApplication().ActiveDocument
+            if (!doc) {
+                alert("当前没有打开任何文档")
+                return
+            } else {
+                if(WAMPO_init()){
+                
+                    wps.PluginStorage.setItem("EnableFlag", true)
+                    wps.ribbonUI.InvalidateControl("New_Chara")
+
+                } else {
+                    alert("123")
+                }
+            }
+            break
+        }
+        case "New_Chara":
+        {
+            wps.ShowDialog(GetUrlPath() + "/ui/dialog.html", "新建人物属性", 400 * window.devicePixelRatio, 400 * window.devicePixelRatio, false)
+            break
+
+        }
+         
+
+        default:
+            break
+
+    }
+    return true
+
+
 }
